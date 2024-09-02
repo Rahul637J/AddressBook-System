c2445fac9fef4f385c29c7cac357a131c867a5e2
Your Name <you@example.com>
2024-09-02 20:32:09 +0530

    [Rahul] Add: Python program to update contact in AddressBook

--- AddressBook.py
+++ AddressBook.py
@@ -24,21 +24,65 @@ class Contact:
 class AddressBook:
     
     def __init__(self):
-        self.contacts=dict()
+        self.contacts=list()
     
-    def add_Contact(self,userName,contact):
-        self.contacts[userName]=contact
-             
+    def add_Contact(self,contact):
+        
+        """
+        Description:
+            Adds a new contact to the address book.
+        
+        Parameters:
+            contact (Contact): A contact object containing contact details like name, address, phone, and email.
+        
+        Return Type:
+            None
+        """
+        
+        self.contacts.append(contact)
+    
+    def edit_contact(self,firstName,new_values):
+        
+        """
+        Description:
+            Edits an existing contact's details in the address book based on the first name provided.
+            Only fields with new values are updated, otherwise, existing data remains unchanged.
+        
+        Parameters:
+            firstName (str): The first name of the contact to be edited.
+            new_values (list): A list containing the new values for the contact fields. If no new value is 
+                               provided for a field, the old value is retained.
+        
+        Return Type:
+            str: A message indicating that the contact information has been updated.
+        """
+        
+        for contact in self.contacts:
+            
+            if firstName == contact.firstName:
+                
+                contact.firstName = new_values[0] or contact.firstName
+                contact.lastName = new_values[1] or contact.lastName
+                contact.address = new_values[2] or contact.address
+                contact.city = new_values[3] or contact.city
+                contact.state = new_values[4] or contact.state
+                contact.zip_code = new_values[5] or contact.zip_code
+                contact.phone = new_values[6] or contact.phone
+                contact.email = new_values[7] or contact.email
+
+            return "User Data Updated!!!"
+            
 def main():
     
-    obj=AddressBook()
+    addressbook_obj=AddressBook()
 
     print("-"*35+"\n| Welcome to Address Book Program |\n"+"-"*35+"\n")
     
     while True:
         option = int(input("Enter :\n"+
                            "       1. Add Contact\n"+
-                           "       2. Exit\n"+
+                           "       2. Edit Contact\n"+
+                           "       3. Exit\n"+
                            "option: "))
         
         if option == 1:
@@ -56,9 +100,29 @@ def main():
 
             user_data=[input(user_input) for user_input in user_data]
             
-            obj.add_Contact(user_data[0],Contact(*user_data))
-
+            addressbook_obj.add_Contact(Contact(*user_data))
+        
         if option == 2:
+            
+            userName=input("Enter your 'First name to edit contact': ")
+            
+            print("Leave the field empty if you don't want to update it.")
+            prompts = [
+                "Enter new First Name: ",
+                "Enter new Last Name: ",
+                "Enter new Address: ",
+                "Enter new City: ",
+                "Enter new State: ",
+                "Enter new Zip code: ",
+                "Enter new Phone Number: ",
+                "Enter new Email: "
+            ]
+
+            new_values = [input(prompt) for prompt in prompts]
+
+            print(addressbook_obj.edit_contact(userName,new_values))
+            
+        if option == 3:
             print("Program exited!!!")
             return    
     
