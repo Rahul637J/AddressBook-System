c928d0101099b3d7833a78a10842815640384c78
Your Name <you@example.com>
2024-09-02 16:53:21 +0530

    [Rahul] Added: Python program to add contact in addressbook

--- AddressBook.py
+++ AddressBook.py
@@ -1,14 +1,52 @@
 '''
+
 @Author: Rahul 
 @Date: 2024-09-02
 @Last Modified by: Rahul 
 @Last Modified time: 2024-09-02
 @Title: Employee wages - Python program to perform phonebook operation.  
+
 '''
 
+class Contact:
+    
+    def __init__(self,firstName,lastname,address,city,state,zip,phone):
+        
+        self.firstName=firstName
+        self.lastName=lastname
+        self.address=address
+        self.city=city
+        self.state=state
+        self.zip=zip
+        self.phone=phone
+
+class AddressBook:
+    
+    def __init__(self):
+        self.contacts=dict()
+    
+    def add_Contact(self,userName,contact):
+        self.contacts[userName]=contact
+             
 def main():
+    
+    obj=AddressBook()
 
-    print("-"*35+"\n| Welcome to Address Book Program |\n"+"-"*35)
+    print("-"*35+"\n| Welcome to Address Book Program |\n"+"-"*35+"\n")
+        
+    user_data=[
+            "Enter your First Name: ",
+            "Enter your Last Name: ",
+            "Enter your Address: ",
+            "Enter your City: ",
+            "Enter your State: ",
+            "Enter the Zip code: ",
+            "Enter your phone number: "
+          ]
 
+    user_data=[input(user_input) for user_input in user_data]
+    
+    obj.add_Contact(Contact(*user_data))
+    
 if __name__=="__main__":
     main()
