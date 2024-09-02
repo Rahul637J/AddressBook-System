c08b8902c4cab4948c629e43882eacb4c3b0a96f
Your Name <you@example.com>
2024-09-02 15:19:43 +0530

    [Rahul] Added: Python program to perform phonebook operation

---
+++ AddressBook.py
@@ -0,0 +1,14 @@
+'''
+@Author: Rahul 
+@Date: 2024-09-02
+@Last Modified by: Rahul 
+@Last Modified time: 2024-09-02
+@Title: Employee wages - Python program to perform phonebook operation.  
+'''
+
+def main():
+
+    print("-"*35+"\n| Welcome to Address Book Program |\n"+"-"*35)
+
+if __name__=="__main__":
+    main()
