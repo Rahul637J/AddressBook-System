93839359a9add45ae9cece6aaf978fb9871eafd0
Your Name <you@example.com>
2024-09-04 12:48:28 +0530

    [Rahul] Added: Python program foradding new contact in addressbook

--- AddressBook.py
+++ AddressBook.py
@@ -4,7 +4,7 @@
 @Date: 2024-09-02
 @Last Modified by: Rahul 
 @Last Modified time: 2024-09-02
-@Title: Employee wages - Python program to perform phonebook operation.  
+@Title: Employee wages - Python program to perform addressbook operation.  
 
 '''
 
@@ -46,33 +46,22 @@ def main():
     obj=AddressBook()
 
     print("-"*35+"\n| Welcome to Address Book Program |\n"+"-"*35+"\n")
-    
-    while True:
-        option = int(input("Enter :\n"+
-                           "       1. Add Contact\n"+
-                           "       2. Exit\n"+
-                           "option: "))
         
-        if option == 1:
             
-            user_data=[
-                "Enter your First Name: ",
-                "Enter your Last Name: ",
-                "Enter your Address: ",
-                "Enter your City: ",
-                "Enter your State: ",
-                "Enter the Zip code: ",
-                "Enter your phone number: ",
-                "Enter your valid email: "
-                ]
+    user_data=[
+        "Enter your First Name: ",
+        "Enter your Last Name: ",
+        "Enter your Address: ",
+        "Enter your City: ",
+        "Enter your State: ",
+        "Enter the Zip code: ",
+        "Enter your phone number: ",
+        "Enter your valid email: "
+        ]
 
-            user_data=[input(user_input) for user_input in user_data]
-            
-            obj.add_Contact(Contact(*user_data))
-
-        if option == 2:
-            print("Program exited!!!")
-            return    
+    user_data=[input(user_input) for user_input in user_data]
+    
+    obj.add_Contact(Contact(*user_data))  
     
 if __name__=="__main__":
     main()
